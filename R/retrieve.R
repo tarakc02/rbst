d8eb8597f1ae27c3bd64fae5bde744b181827514
tarakc02 <tarakc02@github.com>
2015-10-24 19:21:15 -0700

    improve efficiency of retrieve

--- R/retrieve.R
+++ R/retrieve.R
@@ -5,6 +5,7 @@
 #' 
 #' @param tree A \code{bst}
 #' @param key The key to search for
+#' @param ... Ignored
 #' 
 #' @examples
 #' mytree <- bst()
@@ -13,7 +14,7 @@
 #' # can also use subset operator
 #' mytree["a"]
 #' @export
-retrieve <- function(tree, key) UseMethod("retrieve")
+retrieve <- function(tree, key, ...) UseMethod("retrieve")
 
 #' @importFrom assertthat assert_that
 #' @importFrom assertthat is.scalar
@@ -21,17 +22,16 @@ retrieve <- function(tree, key) UseMethod("retrieve")
 retrieve.bst <- function(tree, key) {
     assert_that(is.scalar(key))
     assert_that(!is.na(key))
-    retrieve(tree$root, key)
+    retrieve(tree$root, key, NULL)
 }
 
-retrieve.bstnode <- function(node, key) {
+retrieve.bstnode <- function(node, key, sentinel) {
     x <- compare(key, node$key)
-    if (x < 0)      return(retrieve(node$left, key))
-    else if (x > 0) return(retrieve(node$right, key))
-    else            return(node$value)
+    if (x < 0)      return(retrieve(node$left, key, sentinel))
+    else return(retrieve(node$right, key, node))
 }
 
-retrieve.NULL <- function(node, key) NULL
+retrieve.NULL <- function(node, key, sentinel) sentinel$value
 
 #' @rdname retrieve
 #' @export
