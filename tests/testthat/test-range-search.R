863ac5422de11f53e6751ddb982391c5c36855f4
tarakc02 <tarakc02@github.com>
2015-10-27 10:00:27 -0700

    add more thorough test for keys_between

--- tests/testthat/test-range-search.R
+++ tests/testthat/test-range-search.R
@@ -18,6 +18,22 @@ test_that("keys_between", {
     expect_equal(length(l), 3)    
 })
 
+test_that("keys_between gives same results as manual search", {
+    dummy <- structure(
+        rnorm(1000),
+        names = replicate(1000, paste(sample(letters, 9), collapse =""))
+    )
+    
+    dummytree <- as.bst(dummy)
+    
+    kb1 <- names(dummy[names(dummy) >= "a" & names(dummy) <= "b"])
+    kb2 <- keys_between(dummytree, "a", "b")
+    diff1 <- setdiff(as.character(kb1), as.character(kb2))
+    diff2 <- setdiff(as.character(kb2), as.character(kb1))
+    expect_equal(length(diff1), 0)
+    expect_equal(length(diff2), 0)
+})
+
 test_that("floor and ceiling", {
     tree <- bst()
     tree[1] <- "a"
